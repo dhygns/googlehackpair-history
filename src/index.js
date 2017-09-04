fbfe3f54a39bb2129dbfd16f3213f083f593247b
Donghyeon Kim <dhygns@naver.com>
2017-09-04 21:48:23 +0900

    changed to default style

--- src/index.js
+++ src/index.js
@@ -1,46 +1,5 @@
 
-import Track from "./midi/track.js"
 import Visual from "./visual.js"
-
-
-const button = document.querySelector("button");
-button.addEventListener("click", function(){
-	if (Tone.Transport.state === "started"){
-		// button.textContent = "START";
-		stop();
-	} else {
-		// button.textContent = "STOP";
-		start();
-	}
-});
-
-function stop() {
-	Tone.Transport.stop();
-}
-
-function start() {
-	Tone.Transport.start("+0.1", 0);
-}
-
-
-//Midi file Load... and...
-var Miditracks = [];
-MidiConvert.load("res/MIDI_sample.mid").then(function(midi){
-
-	//You have to check tracks for making notes.
-	console.log(midi);
-
-	// play each tracks with each sound
-	midi.tracks.forEach((track)=>{
-		Miditracks.push(new Track(4, Tone.Synth, track.notes));
-	})
-	
-	Tone.Transport.bpm.value = midi.bpm;
-	Tone.Transport.timeSignature = midi.timeSignature;
-	// button.classList.add("active")
-});
-
-
 
 //Visual Effect
 (function() {
@@ -48,7 +7,7 @@ MidiConvert.load("res/MIDI_sample.mid").then(function(midi){
     this.animate(0, 0);
 }).bind({
     setup : function() {
-        this.main = new Visual(Miditracks);
+        this.main = new Visual();
     },
 
     update : function(t, dt) {
